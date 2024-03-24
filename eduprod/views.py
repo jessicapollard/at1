aa87705462dc1ebc922c98441f42a12cb0a702fb
jessicapollard <jessica.pollard@student.uts.edu.au>
2024-03-24 22:32:24 +1100

    Version 1.2

--- eduprod/views.py
+++ eduprod/views.py
@@ -6,5 +6,6 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def index(request):
     questions = Question.objects.all()
+    questions = Question.objects.order_by('?')[:5]
     questions_json = serializers.serialize('json', questions)
     return render(request, 'eduprod/index.html', {'questions_json': questions_json})
