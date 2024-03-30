df4da3881ab80309606ca8aaf7cdb8c3681c2b6e
jessicapollard <jessica.pollard@student.uts.edu.au>
2024-03-30 14:45:31 +1100

    Version 1.2

--- cellorganelles/views.py
+++ cellorganelles/views.py
@@ -1,3 +1,11 @@
+from django.core import serializers
 from django.shortcuts import render
+from .models import Question
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def index(request):
+    questions = Question.objects.all()
+    questions = Question.objects.order_by('?')[:5]
+    questions_json = serializers.serialize('json', questions)
+    return render(request, 'cellorganelles/index.html', {'questions_json': questions_json})
