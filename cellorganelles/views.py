ffebfc59c3613d0f562d2a84d53a31d65d2023ee
jessicapollard <jessica.pollard@student.uts.edu.au>
2024-04-01 18:15:59 +1100

    Version 1.2

--- cellorganelles/views.py
+++ cellorganelles/views.py
@@ -7,5 +7,6 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     questions = Question.objects.all()
     questions = Question.objects.order_by('?')[:10]
+    # Serialise and use the JSON string directly without additional encoding
     questions_json = serializers.serialize('json', questions)
     return render(request, 'cellorganelles/index.html', {'questions_json': questions_json})
